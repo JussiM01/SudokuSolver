b6f5962484a324fe1b9e2a22e55d9a4d66339f0c
Jussi Martin <jussi.h.a.martin@gmail.com>
2016-07-25 19:31:28 +0300

    Added .gitignore and fixed some typos

--- printsudoku.py
+++ printsudoku.py
@@ -1,12 +1,12 @@
 from sudokusolver import *
 
 def print_table(table):
-    pieciesof3 = [' ' + ' '.join(table[i*3: i*3 + 3]) + ' '
+    pieces_of_3 = [' ' + ' '.join(table[i*3: i*3 + 3]) + ' '
                   for i in range(27)]
-    rowstrings = ['|'.join(pieciesof3[i*3: i*3 + 3]) for i in range(9)]
+    row_strings = ['|'.join(pieces_of_3[i*3: i*3 + 3]) for i in range(9)]
     line = ' ' + '+'.join(['-' * 6, '-' * 7, '-' * 6] )
     for n in range(9):
-        print(' ' * 10 + rowstrings[n])
+        print(' ' * 10 + row_strings[n])
         if n in [2, 5]: print(' ' * 10 + line)
 
 if __name__ == "__main__":
@@ -14,12 +14,12 @@ if __name__ == "__main__":
     print("")
     print("Example:")
     print("")
-    print("Let the the string")
+    print("Let the string")
 
     table = '85...24..72......9..4.........1.7..23.5...9...4...........8..7..17..........36.4.'
 
     print(table)
-    print("present the sudoku table. Then function print_table() will print it as follows:")
+    print("present the sudoku table. Then the function print_table() will print it as follows:")
     print("")
 
     print_table(table)
