4348c3ad104c06c77fe822c6aea141f1e455274d
Jussi Martin <jussi.h.a.martin@gmail.com>
2016-08-04 17:35:42 +0300

    New version of user interface

--- sudokuinterface.py
+++ sudokuinterface.py
@@ -8,15 +8,15 @@ print('Empty squares are presented as dots.')
 table = input('write the string representation here (without quote marks):')
 
 validicity = ['The sudoku is not valid. It dose not have any solutions.',
-    'Uniq solution found. It is:',
-    'The sudoku is not valid. It has multiple solutions']
+    'Solution found. It is:']
 
 def check_validicity(solution):
     if len(solution) == 0: return validicity[0]
-    if len(solution) == 1: return validicity[1]
-    if len(solution) == 2: return validicity[2]
+    return validicity[1]
 
 solution = solve(table)
+print(len(solution))
+print(solution)
 valid = check_validicity(solution)
 print(valid)
 if valid == validicity[1]:
